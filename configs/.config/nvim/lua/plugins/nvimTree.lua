917ce093aaa27043a2b786e0262047647255ba5d
guidipolito <guilhermepereiradipolito@hotmail.com>
2023-04-21 13:16:39 -0300

    Nvim using nvChad base config

--- configs/.config/nvim/lua/plugins/nvimTree.lua
+++
@@ -1,9 +0,0 @@
-require("nvim-web-devicons").setup()
-require("nvim-tree").setup({
-  update_cwd = true,
-  respect_buf_cwd = true,
-  update_focused_file = {
-    enable = true,
-    update_root = true
-  },
-})
